2e67317f64cf20245d39c6bc3bb8a449dd048f69
Larry Lewis <lel486@gmail.com>
2021-08-16 08:09:59 -0400

    remove definition of scid structs from Program clss

--- ReadSierraChartDataSharp/Program.cs
+++ ReadSierraChartDataSharp/Program.cs
@@ -19,57 +19,6 @@ namespace ReadSierraChartDataSharp {
         static readonly Dictionary<char, int> futures_codes = new() { { 'H', 3 }, { 'M', 6 }, { 'U', 9 }, { 'Z', 12 } };
         static readonly DateTime SCDateTimeEpoch = new DateTime(1899, 12, 30, 0, 0, 0, DateTimeKind.Utc); // Sierra Chart SCDateTime start
         static readonly TimeZoneInfo EasternTimeZone = TimeZoneInfo.FindSystemTimeZoneById("US Eastern Standard Time");
-#if False
-        // constructed from Sierra Charts IntradayRecord.h file
-        [StructLayout(LayoutKind.Sequential, Pack = 2)]
-        struct s_IntradayFileHeader {
-            const UInt32 UNIQUE_HEADER_ID = 0x44494353;  // "SCID"
-
-            internal UInt32 FileTypeUniqueHeaderID;  // "SCID"
-            internal UInt32 HeaderSize;
-            internal UInt32 RecordSize;
-            internal UInt16 Version;
-
-            internal void Read(BinaryReader f) {
-                FileTypeUniqueHeaderID = f.ReadUInt32();
-                HeaderSize = f.ReadUInt32();
-                RecordSize = f.ReadUInt32();
-                Version = f.ReadUInt16();
-            }
-        }
-
-        // constructed from Sierra Charts IntradayRecord.h file
-        struct s_IntradayRecord {
-            internal Int64 SCDateTime; // in microseconds since 1899-12-30 00:00:00 UTC
-            internal float Open;
-            internal float High;
-            internal float Low;
-            internal float Close;
-            internal UInt32 NumTrades;
-            internal UInt32 TotalVolume;
-            internal UInt32 BidVolume;
-            internal UInt32 AskVolume;
-
-            internal bool Read(BinaryReader f) {
-                try {
-                    SCDateTime = f.ReadInt64();
-                    Open = f.ReadSingle();
-                    High = f.ReadSingle();
-                    Low = f.ReadSingle();
-                    Close = f.ReadSingle();
-                    NumTrades = f.ReadUInt32();
-                    TotalVolume = f.ReadUInt32();
-                    BidVolume = f.ReadUInt32();
-                    AskVolume = f.ReadUInt32();
-                }
-                catch (IOException) {
-                    return false;
-                }
-
-                return true;
-            }
-        }
-#endif
         enum ReturnCodes {
             Successful,
             MalformedFuturesFileName,
