8204b815494210fe2a6376b4d4828e8003515fe3
Larry Lewis <lel486@gmail.com>
2021-09-01 08:21:27 -0400

    minor changes

--- ReadSierraChartDataSharp/CommandLine.cs
+++ ReadSierraChartDataSharp/CommandLine.cs
@@ -1,57 +1,57 @@
 ﻿//
 // static class CommandLine
-// Proess ReadSierraChartDataSharp command line
+// Process ReadSierraChartDataSharp command line
 //
 
-namespace ReadSierraChartDataSharp {
-    static class CommandLine {
-        internal static void ProcessCommandLineArguments(string[] args) {
-            string? arg_name = null;
+namespace ReadSierraChartDataSharp;
 
-            foreach (string arg in args) {
-                if (arg_name == null) {
-                    switch (arg) {
-                        case "-v":
-                        case "--version":
-                            Console.WriteLine(Program.version);
-                            break;
-                        case "-r":
-                        case "--replace":
-                            Program.update_only = false;
-                            Console.WriteLine(Program.version);
-                            break;
-                        case "-s":
-                        case "--symbol":
-                            arg_name = "-s";
-                            break;
-                        case "-h":
-                        case "--help":
-                            Console.WriteLine(Program.version);
-                            Console.WriteLine("Convert Sierra Chart .scid files into compressed zip files with 3 months data.");
-                            Console.WriteLine("Command line arguments:");
-                            Console.WriteLine("    --version, -v : display version number");
-                            Console.WriteLine("    --update, -u  : only process files input directory which do not have corresponding file in output directory");
-                            Console.WriteLine("    --symbol, -s  : futures contract symbol; i.e. for CME SP500 e-mini: ES");
-                            break;
+static class CommandLine {
+    internal static void ProcessCommandLineArguments(string[] args) {
+        string? arg_name = null;
 
-                        default:
-                            Console.WriteLine("Invalid command line argument: " + arg);
+        foreach (string arg in args) {
+            if (arg_name == null) {
+                switch (arg) {
+                    case "-v":
+                    case "--version":
+                        Console.WriteLine(Program.version);
+                        break;
+                    case "-r":
+                    case "--replace":
+                        Program.update_only = false;
+                        Console.WriteLine(Program.version);
+                        break;
+                    case "-s":
+                    case "--symbol":
+                        arg_name = "-s";
+                        break;
+                    case "-h":
+                    case "--help":
+                        Console.WriteLine(Program.version);
+                        Console.WriteLine("Convert Sierra Chart .scid files into compressed zip files with 3 months data.");
+                        Console.WriteLine("Command line arguments:");
+                        Console.WriteLine("    --version, -v : display version number");
+                        Console.WriteLine("    --update, -u  : only process files input directory which do not have corresponding file in output directory");
+                        Console.WriteLine("    --symbol, -s  : futures contract symbol; i.e. for CME SP500 e-mini: ES");
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid command line argument: " + arg);
+                        System.Environment.Exit(-1);
+                        break;
+                }
+            }
+            else {
+                switch (arg_name) {
+                    case "-s":
+                        if (Program.futures_root.Length > 3) {
+                            Console.WriteLine("Invalid futures contract symbol: " + arg);
                             System.Environment.Exit(-1);
-                            break;
-                    }
+                        }
+                        Program.futures_root = arg.ToUpper();
+                        break;
                 }
-                else {
-                    switch (arg_name) {
-                        case "-s":
-                            if (Program.futures_root.Length > 3) {
-                                Console.WriteLine("Invalid futures contract symbol: " + arg);
-                                System.Environment.Exit(-1);
-                            }
-                            Program.futures_root = arg.ToUpper();
-                            break;
-                    }
-                    arg_name = null;
-                }
+                arg_name = null;
             }
         }
     }
--- ReadSierraChartDataSharp/Scid.cs
+++ ReadSierraChartDataSharp/Scid.cs
@@ -4,86 +4,84 @@
 // The structure definitions are based on the Sierra Chart IntradayRecord.h header file
 
 using System.Runtime.InteropServices;
-using static ReadSierraChartDataSharp.Program;
+//using static ReadSierraChartDataSharp.Program;
+namespace ReadSierraChartDataSharp;
 
-namespace ReadSierraChartDataSharp {
-    class Scid {
-        static readonly DateTime SCDateTimeEpoch = new DateTime(1899, 12, 30, 0, 0, 0, DateTimeKind.Utc); // Sierra Chart SCDateTime start
-        static readonly TimeZoneInfo EasternTimeZone = TimeZoneInfo.FindSystemTimeZoneById("US Eastern Standard Time");
+class Scid {
+    static readonly DateTime SCDateTimeEpoch = new DateTime(1899, 12, 30, 0, 0, 0, DateTimeKind.Utc); // Sierra Chart SCDateTime start
+    static readonly TimeZoneInfo EasternTimeZone = TimeZoneInfo.FindSystemTimeZoneById("US Eastern Standard Time");
 
-        // convert from Sierra Chart DateTime to C# DateTime in Easter US timezone
-        internal static DateTime GetEasternDateTimeFromSCDateTime(Int64 scdt) {
-            // SCDateTime is in microseconds (since 12/30/1899); C# DateTime is in 100 nanoseconds
-            DateTime utc = SCDateTimeEpoch.AddTicks(scdt * 10L);
-            int a = Marshal.SizeOf(typeof(s_IntradayFileHeader));
-            return TimeZoneInfo.ConvertTimeFromUtc(utc, EasternTimeZone);
-        }
+    // convert from Sierra Chart DateTime to C# DateTime in Easter US timezone
+    internal static DateTime GetEasternDateTimeFromSCDateTime(Int64 scdt) {
+        // SCDateTime is in microseconds (since 12/30/1899); C# DateTime is in 100 nanoseconds
+        DateTime utc = SCDateTimeEpoch.AddTicks(scdt * 10L);
+        int a = Marshal.SizeOf(typeof(s_IntradayFileHeader));
+        return TimeZoneInfo.ConvertTimeFromUtc(utc, EasternTimeZone);
     }
+}
 
-    // constructed from Sierra Charts IntradayRecord.h file
-    [StructLayout(LayoutKind.Sequential, Pack = 2)]
-    struct s_IntradayFileHeader {
-        const UInt32 UNIQUE_HEADER_ID = 0x44494353;  // "SCID"
+// constructed from Sierra Charts IntradayRecord.h file
+[StructLayout(LayoutKind.Sequential, Pack = 2)]
+struct s_IntradayFileHeader {
+    const UInt32 UNIQUE_HEADER_ID = 0x44494353;  // "SCID"
 
-        internal UInt32 FileTypeUniqueHeaderID;  // "SCID"
-        internal UInt32 HeaderSize;
-        internal UInt32 RecordSize;
-        internal UInt16 Version;
-        // there's 24 more unused bytes after Version
+    internal UInt32 FileTypeUniqueHeaderID;  // "SCID"
+    internal UInt32 HeaderSize;
+    internal UInt32 RecordSize;
+    internal UInt16 Version;
+    // there's 24 more unused bytes after Version
 
-        internal bool Read(BinaryReader f) {
-            FileTypeUniqueHeaderID = f.ReadUInt32();
-            HeaderSize = f.ReadUInt32();
-            RecordSize = f.ReadUInt32();
-            Version = f.ReadUInt16();
+    internal bool Read(BinaryReader f) {
+        FileTypeUniqueHeaderID = f.ReadUInt32();
+        HeaderSize = f.ReadUInt32();
+        RecordSize = f.ReadUInt32();
+        Version = f.ReadUInt16();
 
-            // skip remaining bytes of header
-            int remaining_bytes = (int)HeaderSize - Marshal.SizeOf(typeof(s_IntradayFileHeader));
-            try {
-                f.ReadBytes(remaining_bytes);
-            }
-            catch (IOException) {
-                Console.WriteLine("IO Error reading header: " + f.ToString());
-                logger?.log(ReturnCodes.IOErrorReadingData, "IO Error: " + f.ToString());
-                return false;
-            }
-
-            return true;
+        // skip remaining bytes of header
+        int remaining_bytes = (int)HeaderSize - Marshal.SizeOf(typeof(s_IntradayFileHeader));
+        try {
+            f.ReadBytes(remaining_bytes);
         }
-    }
-
-    // constructed from Sierra Charts IntradayRecord.h file
-    struct s_IntradayRecord {
-        internal Int64 SCDateTime; // in microseconds since 1899-12-30 00:00:00 UTC
-        internal float Open;
-        internal float High;
-        internal float Low;
-        internal float Close;
-        internal UInt32 NumTrades;
-        internal UInt32 TotalVolume;
-        internal UInt32 BidVolume;
-        internal UInt32 AskVolume;
-
-        internal bool Read(BinaryReader f) {
-            try {
-                SCDateTime = f.ReadInt64();
-                Open = f.ReadSingle();
-                High = f.ReadSingle();
-                Low = f.ReadSingle();
-                Close = f.ReadSingle();
-                NumTrades = f.ReadUInt32();
-                TotalVolume = f.ReadUInt32();
-                BidVolume = f.ReadUInt32();
-                AskVolume = f.ReadUInt32();
-            }
-            catch (IOException) {
-                Console.WriteLine("IO Error reading header: " + f.ToString());
-                logger?.log(ReturnCodes.IOErrorReadingData, "IO Error: " + f.ToString());
-                return false;
-            }
-
-            return true;
+        catch (IOException) {
+            Console.WriteLine("IO Error reading header: " + f.ToString());
+            Program.logger?.log(ReturnCodes.IOErrorReadingData, "IO Error: " + f.ToString());
+            return false;
         }
-    }
 
+        return true;
+    }
+}
+
+// constructed from Sierra Charts IntradayRecord.h file
+struct s_IntradayRecord {
+    internal Int64 SCDateTime; // in microseconds since 1899-12-30 00:00:00 UTC
+    internal float Open;
+    internal float High;
+    internal float Low;
+    internal float Close;
+    internal UInt32 NumTrades;
+    internal UInt32 TotalVolume;
+    internal UInt32 BidVolume;
+    internal UInt32 AskVolume;
+
+    internal bool Read(BinaryReader f) {
+        try {
+            SCDateTime = f.ReadInt64();
+            Open = f.ReadSingle();
+            High = f.ReadSingle();
+            Low = f.ReadSingle();
+            Close = f.ReadSingle();
+            NumTrades = f.ReadUInt32();
+            TotalVolume = f.ReadUInt32();
+            BidVolume = f.ReadUInt32();
+            AskVolume = f.ReadUInt32();
+        }
+        catch (IOException) {
+            Console.WriteLine("IO Error reading header: " + f.ToString());
+            Program.logger?.log(ReturnCodes.IOErrorReadingData, "IO Error: " + f.ToString());
+            return false;
+        }
+
+        return true;
+    }
 }
